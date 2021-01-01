f4337ee25fe4d802be559444bb2435327a076427
S Rodrigues <sr95mail@gmail.com>
2021-01-02 00:55:26 +0100

    Use forked repos instead of ONLYOFFICE repos

--- scripts/base.py
+++ scripts/base.py
@@ -352,9 +352,9 @@ def set_cwd(dir):
 # git ---------------------------------------------------
 def git_update(repo, is_no_errors=False, is_current_dir=False):
   print("[git] update: " + repo)
-  url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  url = "https://github.com/sr95mail/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
-    url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    url = "git@github.com:sr95mail" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
